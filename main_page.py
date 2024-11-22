fd77f2bccafb7f566cfaa4d8184d7cebfa561fbf
Rohith Srinivasan <rohith.srinivasan.wr@renesas.com>
2024-11-22 10:46:17 +0530

    Changes made to hind top bar

--- main_page.py
+++ main_page.py
@@ -18,7 +18,7 @@ hide_st_style = """
             header {visibility: hidden;}
             </style>
             """
-#st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 f.renesas_logo()
 f.header_intro() 
