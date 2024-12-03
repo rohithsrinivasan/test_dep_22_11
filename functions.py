7cceb828a0cb542a7eab6346e6aeba08e9c7134b
Rohith Srinivasan <rohith.srinivasan.wr@renesas.com>
2024-12-03 15:14:04 +0530

    Adding package and Pin details on display+ divider

--- functions.py
+++ functions.py
@@ -69,8 +69,9 @@ def part_number_details(input_part_number,input_buffer):
             st.text(f" User entered Part Number is not matching, please select one from the below")
             part_number, number_of_pins, package_type, package_code = part_number_details_functions.create_selectbox_for_user_to_select(merged_df)
 
-        st.caption(f"Part Number : {part_number}, Number of Pins: {number_of_pins}, Package: {package_type}, Package Code: {package_code}")
-        st.divider()
+        #st.caption(f"Part Number : {part_number}, Number of Pins: {number_of_pins}, Package: {package_type}, Package Code: {package_code}")
+        st.caption(f"Part Info : {number_of_pins} - {package_type}")
+        #st.divider()
 
     return part_number, number_of_pins, package_type, package_code  
 
